262f007278d9d81fe7a2ff340466ccf363437f8c
Daniel Luiz <danielluizthiessen@gmail.com>
2021-06-20 23:13:09 -0300

    Implementei rota games

--- src/app.js
+++ src/app.js
@@ -17,7 +17,10 @@ const connection = new Pool({
 });
 
 let categorias = [];
+let jogos = [];
 let existeCategoria = false;
+let existeIdCategoria = false;
+let existeNomeJogo = false;
 loadCategorias();
 
 async function loadCategorias(){
@@ -36,13 +39,36 @@ function verificarCategoria(categoria){
                 existeCategoria = true;
             }
         }
-
     }
     else{
         existeCategoria = false;
     }
-    
-    
+}
+
+function verificarIdCategoria(id){
+    if(categorias.length != 0){
+        for(let i =0; i<categorias.length; i++){
+            if(id === categorias[i].id){
+                existeIdCategoria = true;
+            }
+        }
+    }
+    else{
+        existeIdCategoria = false;
+    }
+}
+
+function verificarNomeJogo (nome){
+    if(jogos.length != 0){
+        for(let i =0; i<jogos.length; i++){
+            if(nome === jogos[i].name){
+                existeNomeJogo = true;
+            }
+        }
+    }
+    else{
+        existeNomeJogo = false;
+    }
 }
 
 app.get('/categories', (req, res) => {
@@ -72,6 +98,43 @@ app.post('/categories', async (req, res) =>{
         }
     }
     
+});
+
+
+app.get('/games', async (req, res) => {
+    const result = await connection.query('SELECT * FROM games');
+    if(result.row != []){
+        jogos = [...result.rows];
+        console.log(jogos);
+    }
+    res.send(result.rows);
+  });
+
+app.post('/games', async (req, res) => {  
+    verificarIdCategoria(req.body.categoryId);
+    verificarNomeJogo(req.body.name);
+        if(req.body.name == '' || req.body.stockTotal < 0 || req.body.pricePerDay < 0){
+            console.log("N TEM CFUNDA");
+            res.status(400).end();
+            
+        }
+        else if(existeIdCategoria === false){
+            console.log("N TEM CATEGORIA");
+            res.status(400).end();
+        }
+        else if(existeNomeJogo === true){
+            res.status(409).end();
+        }
+        else{
+            try{
+                const result = await connection.query('INSERT INTO games (name , image , "stockTotal" , "categoryId", "pricePerDay") VALUES ($1 , $2 , $3 , $4 , $5)', [req.body.name, req.body.image, req.body.stockTotal, req.body.categoryId, req.body.pricePerDay]);
+                res.status(200).end();
+    
+            }catch(error){
+                console.log(error);
+                res.status(500).end();
+            }
+        }
 });
 
 app.listen(4000, () => {
